f9990dbdecbfdb76ddc1c6496094f699d7f0f395
Fabio Streun <fabiostr@bluewin.ch>
2017-11-27 20:41:48 +0100

    Implemented path handling in the FileHelper

--- app/src/main/java/ch/ethz/inf/vs/fstreun/payapp/filemanager/FileHelper.java
+++ app/src/main/java/ch/ethz/inf/vs/fstreun/payapp/filemanager/FileHelper.java
@@ -2,9 +2,11 @@ package ch.ethz.inf.vs.fstreun.payapp.filemanager;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.util.Log;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -18,7 +20,7 @@ import java.io.OutputStreamWriter;
 
 public class FileHelper {
 
-    Context context;
+    private Context context;
 
     public FileHelper(Context context){
         if (context == null){
@@ -27,44 +29,55 @@ public class FileHelper {
         this.context = context;
     }
 
-    public void writeToFile(String fileName, String data){
-        FileOutputStream outputStream;
+    public boolean writeToFile(@Nullable String path, String fileName, String data){
         try {
-            outputStream = context.openFileOutput(fileName, Context.MODE_PRIVATE);
-            toFile(outputStream, data);
-        } catch (FileNotFoundException e) {
-            // Not able to create a file
-            // If it can be created it will create a file!
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
+            return toFile(path, fileName, false, data);
+        } catch (IOException e) {
+            return false;
         }
     }
 
-    public void addToFile(String fileName, String data){
-        FileOutputStream outputStream;
+    public boolean appendToFile(String path, String fileName, String data){
         try {
-            outputStream = context.openFileOutput(fileName, Context.MODE_APPEND);
-            toFile(outputStream, data);
-        } catch (FileNotFoundException e) {
-            // Not able to create a file
-            // If it can be created it will create a file!
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
+            return toFile(path, fileName, true, data);
+        } catch (IOException e) {
+            return false;
         }
     }
 
-    private void toFile(FileOutputStream outputStream, String data) throws IOException {
+    private boolean toFile(@Nullable String path, String fileName, boolean append, String data) throws IOException {
+        if (path == null){
+            path = "";
+        }
+
+        File directory = new File(context.getFilesDir(), path);
+        //Creating an internal dir;
+        if (!directory.exists()) {
+            if (!directory.mkdirs()) {
+                return false;
+            }
+        }
+        File file = new File(directory, fileName); //Getting a file within the dir.
+        FileOutputStream outputStream = new FileOutputStream(file, append); //Use the stream as usual to write into the file.
         outputStream.write(data.getBytes());
         outputStream.close();
+        return true;
     }
 
-    public String readFromFile(String fileName) throws FileNotFoundException {
-        //TODO: read file by lines will cause a end line at the end of file...
-        FileInputStream inputStream;
+    public String readFromFile(@Nullable String path, String fileName) throws FileNotFoundException {
+        if (path == null){
+            path = "";
+        }
+        File directory = new File(context.getFilesDir(), path); //Creating an internal dir;
+        if (!directory.exists()){
+            throw new FileNotFoundException("Path does not exist: " + path);
+        }
+        File file = new File(directory, fileName); //Getting a file within the dir.
+        if (!file.exists()){
+            throw new FileNotFoundException("File does not exist: " + path + "/" + fileName);
+        }
 
-        inputStream = context.openFileInput(fileName);
+        FileInputStream inputStream = new FileInputStream(file);
 
         InputStreamReader isr = new InputStreamReader(inputStream);
         BufferedReader bufferedReader = new BufferedReader(isr);
@@ -72,6 +85,7 @@ public class FileHelper {
         String line;
 
         try {
+            //TODO: read file by lines will cause a end line at the end of file...
             while ((line = bufferedReader.readLine()) != null) {
                 sb.append(line+"\n");
             }
