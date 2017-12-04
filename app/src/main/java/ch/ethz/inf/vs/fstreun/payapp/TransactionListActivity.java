ae014f42cecc3e489cb74cf799a4fe75d1a4a301
antonbr <antonbr@student.ethz.ch>
2017-12-04 23:16:46 +0100

    TransactionListActivity done

--- app/src/main/java/ch/ethz/inf/vs/fstreun/payapp/TransactionListActivity.java
+++ app/src/main/java/ch/ethz/inf/vs/fstreun/payapp/TransactionListActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -22,20 +23,23 @@ import ch.ethz.inf.vs.fstreun.payapp.filemanager.FileHelper;
 public class TransactionListActivity extends AppCompatActivity {
 
     public final static String KEY_PARTICIPANT = "participant"; // String
-    public final static String KEY_GROUP_NAME = "group"; // String
+    public final static String KEY_GROUP_NAME = "group_name"; // String
     public final static String KEY_FILTER_TYPE = "filter_type"; // String
         // NO_FILTER (all transactions); PAID_BY_NAME; NAME_INVOLVED
-    public final static String KEY_GROUP_ID = "filter_type"; // String
+        // see R.string.filter_* for definitions
+    public final static String KEY_GROUP_ID = "group_id"; // String
 
     private String participantName;
     private String groupName;
     private UUID groupID;
-    private String type;
     private Group group;
 
     // also referred to by the adapter
     List<Transaction> transactionList = new ArrayList<>();
     ListView listView;
+
+    // Filter Buttons
+    Button btnAll, btnPaid, btnInvolved;
 
     String TAG = "###TransactionListActivity";
 
@@ -44,14 +48,56 @@ public class TransactionListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaction_list);
 
+        // initialize buttons
+        btnAll = findViewById(R.id.btn_show_all_transaction);
+        btnAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //todo: make this button marked and the others not marked
+                updateListView(getFilteredList(getString(R.string.filter_no_filter)));
+            }
+        });
+        btnPaid = findViewById(R.id.btn_show_paid_by);
+        btnPaid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //todo: make this button marked and the others not marked
+                updateListView(getFilteredList(getString(R.string.filter_paid_by_name)));
+            }
+        });
+        btnInvolved = findViewById(R.id.btn_show_involved);
+        btnInvolved.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //todo: make this button marked and the others not marked
+                updateListView(getFilteredList(getString(R.string.filter_name_involved)));
+            }
+        });
+
+        //initialize listView
+        listView = findViewById(R.id.listView_transaction);
+
         //get information from intent
         Intent intent = getIntent();
         groupName = intent.getStringExtra(KEY_GROUP_NAME);
         groupID = UUID.fromString(intent.getStringExtra(KEY_GROUP_ID));
-        type = intent.getStringExtra(KEY_FILTER_TYPE);
+        String type = intent.getStringExtra(KEY_FILTER_TYPE);
+        Log.d(TAG, "type = " + type);
+        Log.d(TAG, "groupName = " + groupName);
+        Log.d(TAG, "groupID = " + groupID.toString());
 
         //set title
         setTitle("Transactions in " + groupName);
+
+        //get payer name
+        participantName = intent.getStringExtra(KEY_PARTICIPANT);
+
+        //set button text
+        if(participantName != null){
+            //todo: buttons are moved down a bit when name is too long
+            btnPaid.setText("Paid by " + participantName);
+            btnInvolved.setText("" + participantName + " involved");
+        }
 
         // if no type defined, show all transactions
         if (type == null)
@@ -72,50 +118,52 @@ public class TransactionListActivity extends AppCompatActivity {
         //make sure Group object is not null
         if (group == null) onDestroy();
 
+        // call filter function
+        transactionList = getFilteredList(type);
 
+        updateListView(transactionList);
+    }
+
+    private List<Transaction> getFilteredList(String type) {
         //-----------------------------------------------------------------
         //type case distinction
-        // TODO: move to special method which takes the filter string and changes the transactionList
         //-----------------------------------------------------------------
+
+        List<Transaction> result = new ArrayList<>();
 
         Log.d(TAG, "entering type case distinction with type " + type);
         //type: all transactions
         if (type.equals(getString(R.string.filter_no_filter))){
-            transactionList = group.getTransactions();
+            result = group.getTransactions();
 
-        //type: filter paid by
+            //type: filter paid by
         } else if (type.equals(getString(R.string.filter_paid_by_name))) {
-            //get payer name
-            participantName = intent.getStringExtra(KEY_PARTICIPANT);
-
             //iterate over all transactions of this group
             for (Transaction t : group.getTransactions()){
                 if (participantName.equals(t.getPayer())){
                     Log.d(TAG, "adding a transaction");
-                    transactionList.add(t);
+                    result.add(t);
                 }
             }
 
-        //type: all involved (and paid)
+            //type: all involved (and paid)
         } else if (type.equals(getString(R.string.filter_name_involved))){
-            //get name
-            participantName = intent.getStringExtra(KEY_PARTICIPANT);
-
             //iterate over all transactions of this group
             for (Transaction t : group.getTransactions()){
                 if (t.getInvolved().contains(participantName) || participantName.equals(t.getPayer())){
                     Log.d(TAG, "adding a transaction");
-                    transactionList.add(t);
+                    result.add(t);
                 }
             }
 
         }
 
-        //todo: take out nextline
-        transactionList = group.getTransactions();
+        return result;
 
+    }
+
+    private void updateListView(List<Transaction> transactionList) {
         ListTransactionAdapter adapter = new ListTransactionAdapter(this, transactionList);
-        listView = findViewById(R.id.listView_transaction);
         listView.setAdapter(adapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -126,4 +174,5 @@ public class TransactionListActivity extends AppCompatActivity {
             }
         });
     }
+
 }
